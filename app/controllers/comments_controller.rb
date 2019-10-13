a627b3645d810c2fdf9fb8800fd81ee05a89de20
maxPiroddi <mpiroddi@outlook.com>
2019-10-13 14:19:11 +1100

    Cleaning up controller code & creating comment functions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,47 @@
 class CommentsController < ApplicationController
+    before_action :find_message
+    before_action :find_comment, only: [:edit, :update, :destroy]
 
     def create
-        # First we find the message using message ID
-        @message = Message.find(params[:message_id])
         # Then we create a comment with respect to said message
         @comment = @message.comments.create(comment_params)
+        # Finally we stick the current user ID to the comment
+        @comment.user_id = current_user.id 
+
+        if @comment.save
+            redirect_to message_path(@message)
+        else
+            render "new"
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @comment.update(comment_params)
+            redirect_to message_path(@message)
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+
     end
 
     private
-
+    # Keeping code DRY
         def comment_params
             params.require(:comment).permit(:content)
+        end
+
+        def find_message
+            @message = Message.find(params[:message_id])
+        end
+
+        def find_comment
+            @comment = @message.comments.find(params[:id])
         end
 
 end
